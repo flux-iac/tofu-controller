971bff97a265f50c8af71bb6550b933ef55e7554
Chanwit Kaewkasi <chanwit@gmail.com>
2021-12-27 18:41:43 +0700

    add test for vars selection by varsfrom's varskeys

---
+++ controllers/tc000071_varsfrom_secret_with_varkeys_and_controlled_outputs_test.go
@@ -0,0 +1,156 @@
+package controllers
+
+import (
+	"context"
+	infrav1 "github.com/chanwit/tf-controller/api/v1alpha1"
+	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"testing"
+	"time"
+)
+
+// +kubebuilder:docs-gen:collapse=Imports
+
+func Test_000071_varsfrom_secret_with_varkeys_and_controlled_outputs_test(t *testing.T) {
+	const (
+		sourceName    = "tf-vars-from-with-varkeys-controlled-output"
+		terraformName = "helloworld-vars-from-with-varkeys-controlled-output"
+	)
+	g := NewWithT(t)
+	ctx := context.Background()
+
+	by("creating a new Git repository object")
+	updatedTime := time.Now()
+	testRepo := sourcev1.GitRepository{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      sourceName,
+			Namespace: "flux-system",
+		},
+		Spec: sourcev1.GitRepositorySpec{
+			URL: "https://github.com/openshift-fluxv2-poc/podinfo",
+			Reference: &sourcev1.GitRepositoryRef{
+				Branch: "master",
+			},
+			Interval:          metav1.Duration{Duration: time.Second * 30},
+			GitImplementation: "go-git",
+		},
+	}
+	g.Expect(k8sClient.Create(ctx, &testRepo)).Should(Succeed())
+
+	by("setting the git repo status object, the URL, and the correct checksum")
+	testRepo.Status = sourcev1.GitRepositoryStatus{
+		ObservedGeneration: int64(1),
+		Conditions: []metav1.Condition{
+			{
+				Type:               "Ready",
+				Status:             metav1.ConditionTrue,
+				LastTransitionTime: metav1.Time{Time: updatedTime},
+				Reason:             "GitOperationSucceed",
+				Message:            "Fetched revision: master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			},
+		},
+		URL: server.URL() + "/env.tar.gz",
+		Artifact: &sourcev1.Artifact{
+			Path:           "gitrepository/flux-system/test-tf-controller/b8e362c206e3d0cbb7ed22ced771a0056455a2fb.tar.gz",
+			URL:            server.URL() + "/env.tar.gz",
+			Revision:       "master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			Checksum:       "d021eda9b869586f5a43ad1ba7f21e4bf9b3970443236755463f22824b525316",
+			LastUpdateTime: metav1.Time{Time: updatedTime},
+		},
+	}
+	g.Expect(k8sClient.Status().Update(ctx, &testRepo)).Should(Succeed())
+
+	by("checking that the status and its URL gets reconciled")
+	gitRepoKey := types.NamespacedName{Namespace: "flux-system", Name: sourceName}
+	createdRepo := &sourcev1.GitRepository{}
+	g.Expect(k8sClient.Get(ctx, gitRepoKey, createdRepo)).Should(Succeed())
+
+	by("preparing my-vars secret")
+	myVars := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-vars-" + terraformName,
+			Namespace: "flux-system",
+		},
+		Data: map[string][]byte{
+			"subject": []byte("my secret cat"),
+			"unused":  []byte("unused value"),
+		},
+		Type: corev1.SecretTypeOpaque,
+	}
+	g.Expect(k8sClient.Create(ctx, &myVars)).Should(Succeed())
+
+	by("creating a new TF and attaching to the repo")
+	helloWorldTF := infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      terraformName,
+			Namespace: "flux-system",
+		},
+		Spec: infrav1.TerraformSpec{
+			ApprovePlan: "auto",
+			Path:        "./terraform-hello-env",
+			SourceRef: infrav1.CrossNamespaceSourceReference{
+				Kind:      "GitRepository",
+				Name:      sourceName,
+				Namespace: "flux-system",
+			},
+			// TODO change to a better type
+			VarsFrom: &infrav1.VarsReference{
+				Kind:     "Secret",
+				Name:     "my-vars-" + terraformName,
+				VarsKeys: []string{"subject"},
+			},
+			WriteOutputsToSecret: &infrav1.WriteOutputsToSecretSpec{
+				Name: "tf-output-" + terraformName,
+				Outputs: []string{
+					"hello_world",
+				},
+			},
+		},
+	}
+	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
+
+	by("checking that the hello world TF got created")
+	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
+	createdHelloWorldTF := infrav1.Terraform{}
+	// We'll need to retry getting this newly created Terraform, given that creation may not immediately happen.
+	g.Eventually(func() bool {
+		err := k8sClient.Get(ctx, helloWorldTFKey, &createdHelloWorldTF)
+		if err != nil {
+			return false
+		}
+		return true
+	}, timeout, interval).Should(BeTrue())
+
+	by("checking that the TF output secret contains a binary data")
+	outputKey := types.NamespacedName{Namespace: "flux-system", Name: "tf-output-" + terraformName}
+	outputSecret := corev1.Secret{}
+	g.Eventually(func() (int, error) {
+		err := k8sClient.Get(ctx, outputKey, &outputSecret)
+		if err != nil {
+			return -1, err
+		}
+		return len(outputSecret.Data), nil
+	}, timeout, interval).Should(Equal(1))
+
+	by("checking that the TF output secrets contains the correct output provisioned by the TF hello world")
+	// Value is a JSON representation of TF's OutputMeta
+	expectedOutputValue := map[string]string{
+		"Name":        "tf-output-" + terraformName,
+		"Namespace":   "flux-system",
+		"Value":       "{\"sensitive\":false,\"type\":\"string\",\"value\":\"Hello, my secret cat!\"}",
+		"OwnerRef[0]": string(createdHelloWorldTF.UID),
+	}
+	g.Eventually(func() (map[string]string, error) {
+		err := k8sClient.Get(ctx, outputKey, &outputSecret)
+		value := string(outputSecret.Data["hello_world"])
+		return map[string]string{
+			"Name":        outputSecret.Name,
+			"Namespace":   outputSecret.Namespace,
+			"Value":       value,
+			"OwnerRef[0]": string(outputSecret.OwnerReferences[0].UID),
+		}, err
+	}, timeout, interval).Should(Equal(expectedOutputValue), "expected output %v", expectedOutputValue)
+}
