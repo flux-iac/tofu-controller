dc17fa723673a22a30e07d77f6abb61a707866e4
Piaras Hoban <piaras@weave.works>
2022-01-18 10:35:32 +0000

    tests: add terraform module to test varsFrom and update tests to use same
    Signed-off-by: Piaras Hoban <piaras@weave.works>

--- controllers/tc000081_varsfrom_accepts_many_configmap_controlled_outputs_test.go
+++ controllers/tc000081_varsfrom_accepts_many_configmap_controlled_outputs_test.go
@@ -54,12 +54,12 @@ func Test_000081_varsfrom_accepts_many_configMaps(t *testing.T) {
 				Message:            "Fetched revision: master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
 			},
 		},
-		URL: server.URL() + "/env.tar.gz",
+		URL: server.URL() + "/tf-multi-var.tar.gz",
 		Artifact: &sourcev1.Artifact{
 			Path:           "gitrepository/flux-system/test-tf-controller/b8e362c206e3d0cbb7ed22ced771a0056455a2fb.tar.gz",
-			URL:            server.URL() + "/env.tar.gz",
+			URL:            server.URL() + "/tf-multi-var.tar.gz",
 			Revision:       "master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
-			Checksum:       "d021eda9b869586f5a43ad1ba7f21e4bf9b3970443236755463f22824b525316",
+			Checksum:       "52fbbf10455df51136a0c43e0f548c01acdbafca5cbad12c787612e47a4aa815",
 			LastUpdateTime: metav1.Time{Time: updatedTime},
 		},
 	}
@@ -71,72 +71,82 @@ func Test_000081_varsfrom_accepts_many_configMaps(t *testing.T) {
 	g.Expect(k8sClient.Get(ctx, gitRepoKey, createdRepo)).Should(Succeed())
 
 	By("preparing vars configMaps")
-	for name, value := range map[string]string{
-		"config-1": "config-1-value",
-		"config-2": "config-2-value",
-	} {
+	cmData := []struct {
+		name string
+		data map[string]string
+	}{
+		{
+			name: "cm1",
+			data: map[string]string{
+				"cluster_name": "felix",
+			},
+		},
+		{
+			name: "cm2",
+			data: map[string]string{
+				"region":      "eu-west-1",
+				"environment": "dev",
+			},
+		},
+	}
+	for _, cm := range cmData {
 		configMap := &corev1.ConfigMap{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      name,
+				Name:      cm.name,
 				Namespace: "flux-system",
 			},
-			Data: map[string]string{
-				"subject": value,
-			},
+			Data: cm.data,
 		}
 		g.Expect(k8sClient.Create(ctx, configMap)).Should(Succeed())
 	}
 
 	By("creating a new TF and attaching to the repo")
-	helloWorldTF := infrav1.Terraform{
+	testTF := infrav1.Terraform{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      terraformName,
 			Namespace: "flux-system",
 		},
 		Spec: infrav1.TerraformSpec{
 			ApprovePlan: "auto",
-			Path:        "./terraform-hello-env",
+			Path:        "./tf-multi-var-with-outputs",
 			SourceRef: infrav1.CrossNamespaceSourceReference{
 				Kind:      "GitRepository",
 				Name:      sourceName,
 				Namespace: "flux-system",
 			},
-			Vars: []infrav1.Variable{
-				{Name: "subject", Value: "my cat"},
-			},
 			VarsFrom: []infrav1.VarsReference{
 				{
 					Kind: "ConfigMap",
-					Name: "config-1",
+					Name: "cm1",
 				},
 				{
-					Kind: "ConfigMap",
-					Name: "config-2",
+					Kind:     "ConfigMap",
+					Name:     "cm2",
+					VarsKeys: []string{"environment", "region"},
 				},
 			},
 			WriteOutputsToSecret: &infrav1.WriteOutputsToSecretSpec{
 				Name: "tf-output-" + terraformName,
 				Outputs: []string{
-					"hello_world",
+					"cluster_id",
 				},
 			},
 		},
 	}
-	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
+	g.Expect(k8sClient.Create(ctx, &testTF)).Should(Succeed())
 
-	By("checking that the hello world TF got created")
-	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
-	createdHelloWorldTF := infrav1.Terraform{}
-	// We'll need to retry getting this newly created Terraform, Given that creation may not immediately happen.
+	By("checking that the terraform resource got created")
+	testTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
+	testTFInstance := infrav1.Terraform{}
 	g.Eventually(func() bool {
-		err := k8sClient.Get(ctx, helloWorldTFKey, &createdHelloWorldTF)
+		err := k8sClient.Get(ctx, testTFKey, &testTFInstance)
 		if err != nil {
 			return false
 		}
 		return true
 	}, timeout, interval).Should(BeTrue())
 
-	By("checking that the TF output configMap contains a binary data")
+	By("checking that the TF output secret contains binary data")
 	outputKey := types.NamespacedName{Namespace: "flux-system", Name: "tf-output-" + terraformName}
 	outputSecret := corev1.Secret{}
 	g.Eventually(func() (int, error) {
@@ -147,17 +157,17 @@ func Test_000081_varsfrom_accepts_many_configMaps(t *testing.T) {
 		return len(outputSecret.Data), nil
 	}, timeout, interval).Should(Equal(1))
 
-	By("checking that the TF output secret contains the correct output provisioned by the TF hello world")
+	By("checking that the TF output secret contains the correct output provisioned by the TF resource")
 	// Value is a JSON representation of TF's OutputMeta
 	expectedOutputValue := map[string]string{
 		"Name":        "tf-output-" + terraformName,
 		"Namespace":   "flux-system",
-		"Value":       "Hello, config-2-value!",
-		"OwnerRef[0]": string(createdHelloWorldTF.UID),
+		"Value":       "dev-eu-west-1-felix",
+		"OwnerRef[0]": string(testTFInstance.UID),
 	}
 	g.Eventually(func() (map[string]string, error) {
 		err := k8sClient.Get(ctx, outputKey, &outputSecret)
-		value := string(outputSecret.Data["hello_world"])
+		value := string(outputSecret.Data["cluster_id"])
 		return map[string]string{
 			"Name":        outputSecret.Name,
 			"Namespace":   outputSecret.Namespace,
