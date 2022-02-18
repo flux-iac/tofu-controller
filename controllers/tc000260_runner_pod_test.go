8e6b060faa6c9472df572ec6e383a027c7fdf784
Tom Huang <tom.huang@weave.works>
2022-02-18 16:51:56 -0500

    improve test coverage
    Signed-off-by: Tom Huang <tom.huang@weave.works>

---
+++ controllers/tc000260_runner_pod_test.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	infrav1 "github.com/weaveworks/tf-controller/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:docs-gen:collapse=Imports
+
+func Test_000260_runner_pod_test(t *testing.T) {
+	Spec("This spec describes a runner pod creation process")
+
+	const (
+		terraformName      = "runner-pod-test"
+		sourceName         = "runner-pod-test"
+		serviceAccountName = "helloworld-tf-runner"
+	)
+
+	g := NewWithT(t)
+
+	It("shoud reconcile a runner pod")
+	By("pass a terraform object, the runner pod spec should be accurate")
+	helloWorldTF := infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      terraformName,
+			Namespace: "flux-system",
+		},
+		Spec: infrav1.TerraformSpec{
+			ApprovePlan: "auto",
+			Path:        "./terraform-hello-world-example",
+			SourceRef: infrav1.CrossNamespaceSourceReference{
+				Kind:      "GitRepository",
+				Name:      sourceName,
+				Namespace: "flux-system",
+			},
+			ServiceAccountName: serviceAccountName,
+		},
+	}
+
+	spec := runnerPodSpec(helloWorldTF)
+	g.Expect(spec.ServiceAccountName == serviceAccountName)
+}
