5bf9618022eb5153b159ede2a1e6a5d7ab203613
Piaras Hoban <phoban01@gmail.com>
2022-03-10 10:10:51 +0000

    fix: ensure pre connect tls validation uses host without port
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- api/v1alpha1/terraform_types.go
+++ api/v1alpha1/terraform_types.go
@@ -157,7 +157,7 @@ type TerraformSpec struct {
 	AlwaysCleanupRunnerPod bool `json:"alwaysCleanupRunnerPod,omitempty"`
 
 	// Configure the termination grace period for the runner pod. Use this parameter
-	// to allow the terraform process to gracefully shutdown. Consider increasing for
+	// to allow the Terraform process to gracefully shutdown. Consider increasing for
 	// large, complex or slow-moving Terraform managed resources.
 	// +kubebuilder:default:=30
 	// +optional
