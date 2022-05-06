36077f2118913fc0a8603b666d4d91f721e961d1
Luke Mallon (Nalum) <luke.mallon@weave.works>
2022-05-06 16:18:09 +0100

    Allow use of EnvFrom as well
    Signed-off-by: Luke Mallon (Nalum) <luke.mallon@weave.works>

--- api/v1alpha1/reference_types.go
+++ api/v1alpha1/reference_types.go
@@ -125,6 +125,15 @@ type RunnerPodSpec struct {
 	// +optional
 	Image string `json:"image,omitempty"`
 
+	// List of sources to populate environment variables in the container.
+	// The keys defined within a source must be a C_IDENTIFIER. All invalid keys
+	// will be reported as an event when the container is starting. When a key exists in multiple
+	// sources, the value associated with the last source will take precedence.
+	// Values defined by an Env with a duplicate key will take precedence.
+	// Cannot be updated.
+	// +optional
+	EnvFrom []corev1.EnvFromSource `json:"envFrom,omitempty"`
+
 	// List of environment variables to set in the container.
 	// Cannot be updated.
 	// +optional
