bacdbcab76ae9dd751f08684aaa65566a117e64e
Luke Mallon (Nalum) <luke.mallon@weave.works>
2022-05-10 13:05:09 +0100

    Add test checking the envvar gets from Terraform resource to Secret Output
    Signed-off-by: Luke Mallon (Nalum) <luke.mallon@weave.works>

--- controllers/tc000260_runner_pod_test.go
+++ controllers/tc000260_runner_pod_test.go
@@ -1,13 +1,17 @@
 package controllers
 
 import (
+	"context"
 	"os"
 	"testing"
+	"time"
 
+	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	. "github.com/onsi/gomega"
 	infrav1 "github.com/weaveworks/tf-controller/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // +kubebuilder:docs-gen:collapse=Imports
@@ -329,4 +333,148 @@ func Test_000260_runner_pod_test_env_vars_proxy_overwrite(t *testing.T) {
 		}
 		return true
 	})
+}
+
+func Test_000260_runner_pod_test_env_vars_proxy_output(t *testing.T) {
+	Spec("This spec describes the behaviour of a Terraform resource when variables are provided via EnvVars.")
+	It("should be reconciled and output the variable in an output.")
+
+	const (
+		sourceName    = "gr-envvars-variable-output"
+		terraformName = "tf-envvars-variable-output"
+	)
+	g := NewWithT(t)
+	ctx := context.Background()
+
+	Given("a GitRepository")
+	By("defining a new GitRepository resource.")
+	updatedTime := time.Now()
+	testRepo := sourcev1.GitRepository{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      sourceName,
+			Namespace: "flux-system",
+		},
+		Spec: sourcev1.GitRepositorySpec{
+			URL: "https://github.com/openshift-fluxv2-poc/podinfo",
+			Reference: &sourcev1.GitRepositoryRef{
+				Branch: "master",
+			},
+			Interval:          metav1.Duration{Duration: time.Second * 30},
+			GitImplementation: "go-git",
+		},
+	}
+
+	By("creating the GitRepository resource in the cluster.")
+	It("should be created successfully.")
+	g.Expect(k8sClient.Create(ctx, &testRepo)).Should(Succeed())
+	defer func() { g.Expect(k8sClient.Delete(ctx, &testRepo)).Should(Succeed()) }()
+
+	Given("the GitRepository's reconciled status")
+	By("setting the GitRepository's status, with the downloadable BLOB's URL, and the correct checksum.")
+	testRepo.Status = sourcev1.GitRepositoryStatus{
+		ObservedGeneration: int64(1),
+		Conditions: []metav1.Condition{
+			{
+				Type:               "Ready",
+				Status:             metav1.ConditionTrue,
+				LastTransitionTime: metav1.Time{Time: updatedTime},
+				Reason:             "GitOperationSucceed",
+				Message:            "Fetched revision: master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			},
+		},
+		URL: server.URL() + "/terraform-envvars-variable-output.tar.gz",
+		Artifact: &sourcev1.Artifact{
+			Path:           "gitrepository/flux-system/test-tf-controller/b8e362c206e3d0cbb7ed22ced771a0056455a2fb.tar.gz",
+			URL:            server.URL() + "/terraform-envvars-variable-output.tar.gz",
+			Revision:       "master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			Checksum:       "1329c5b6743c8115f17782c8f4ad89ddf1279e41ed33cb1cba5491cc31c02863",
+			LastUpdateTime: metav1.Time{Time: updatedTime},
+		},
+	}
+
+	It("should be updated successfully.")
+	g.Expect(k8sClient.Status().Update(ctx, &testRepo)).Should(Succeed())
+
+	By("checking that the status and its URL gets reconciled.")
+	gitRepoKey := types.NamespacedName{Namespace: "flux-system", Name: sourceName}
+	createdRepo := &sourcev1.GitRepository{}
+	g.Expect(k8sClient.Get(ctx, gitRepoKey, createdRepo)).Should(Succeed())
+
+	Given("a Terraform resource with auto approve, attached to the given GitRepository resource")
+	By("creating a new TF resource and attaching to the repo via `sourceRef`.")
+	By("specifying the .spec.writeOutputsToSecret.")
+
+	helloWorldTF := infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      terraformName,
+			Namespace: "flux-system",
+		},
+		Spec: infrav1.TerraformSpec{
+			ApprovePlan: "auto",
+			Path:        "./terraform-envvars-variable-output",
+			SourceRef: infrav1.CrossNamespaceSourceReference{
+				Kind:      "GitRepository",
+				Name:      sourceName,
+				Namespace: "flux-system",
+			},
+			RunnerPodTemplate: infrav1.RunnerPodTemplate{
+				Spec: infrav1.RunnerPodSpec{
+					Env: []corev1.EnvVar{
+						{
+							Name:  "TF_VAR_test_env_var",
+							Value: "TEST_ENVVAR_VALUE",
+						},
+					},
+				},
+			},
+			Interval: metav1.Duration{Duration: time.Second * 10},
+			WriteOutputsToSecret: &infrav1.WriteOutputsToSecretSpec{
+				Name: terraformName,
+			},
+		},
+	}
+	It("should be created and attached successfully.")
+	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
+	defer func() { g.Expect(k8sClient.Delete(ctx, &helloWorldTF)).Should(Succeed()) }()
+
+	By("checking that the TF resource existed inside the cluster.")
+	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
+	createdHelloWorldTF := infrav1.Terraform{}
+	g.Eventually(func() bool {
+		err := k8sClient.Get(ctx, helloWorldTFKey, &createdHelloWorldTF)
+		if err != nil {
+			return false
+		}
+		return true
+	}, timeout, interval).Should(BeTrue())
+
+	It("should be reconciled and produce the correct output secret.")
+	By("checking that the named output secret contains a binary data.")
+	outputKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
+	outputSecret := corev1.Secret{}
+	g.Eventually(func() (int, error) {
+		err := k8sClient.Get(ctx, outputKey, &outputSecret)
+		if err != nil {
+			return -1, err
+		}
+		return len(outputSecret.Data), nil
+	}, timeout, interval).Should(Equal(1))
+
+	By("checking that the output secret contains the correct output data, provisioned by the TF resource.")
+	expectedOutputValue := map[string]string{
+		"Name":        terraformName,
+		"Namespace":   "flux-system",
+		"Value":       "TEST_ENVVAR_VALUE",
+		"OwnerRef[0]": string(createdHelloWorldTF.UID),
+	}
+	g.Eventually(func() (map[string]string, error) {
+		err := k8sClient.Get(ctx, outputKey, &outputSecret)
+		value := string(outputSecret.Data["test_env_var"])
+		return map[string]string{
+			"Name":        outputSecret.Name,
+			"Namespace":   outputSecret.Namespace,
+			"Value":       value,
+			"OwnerRef[0]": string(outputSecret.OwnerReferences[0].UID),
+		}, err
+	}, timeout, interval).Should(Equal(expectedOutputValue), "expected output %v", expectedOutputValue)
 }
