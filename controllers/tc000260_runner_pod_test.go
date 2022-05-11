57d07f6a6fdae4941529980c70bf282a39e57fdb
Luke Mallon (Nalum) <luke.mallon@weave.works>
2022-05-11 11:01:10 +0100

    Fix name matching for secret test
    Signed-off-by: Luke Mallon (Nalum) <luke.mallon@weave.works>

--- controllers/tc000260_runner_pod_test.go
+++ controllers/tc000260_runner_pod_test.go
@@ -471,7 +471,7 @@ func Test_000260_runner_pod_test_envvar_proxy_output(t *testing.T) {
 
 	By("checking that the output secret contains the correct output data, provisioned by the TF resource.")
 	expectedOutputValue := map[string]string{
-		"Name":              terraformName,
+		"Name":              terraformNameSecret,
 		"Namespace":         "flux-system",
 		"Value HTTPS_PROXY": "http://test.proxy:1234",
 		"Value HTTP_PROXY":  "http://test.proxy:1234",
