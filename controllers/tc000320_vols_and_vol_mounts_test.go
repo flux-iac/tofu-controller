f3937074b70e405d834cc2dff979abc930352059
Steven Fraser <steve.fraser@weave.works>
2022-09-02 12:23:06 -0700

    adding test with no additional vol mounts

--- controllers/tc000320_vols_and_vol_mounts_test.go
+++ controllers/tc000320_vols_and_vol_mounts_test.go
@@ -254,3 +254,114 @@ func Test_000320_vol_mount_test(t *testing.T) {
 		return len(runnerPod.Spec.Containers[0].VolumeMounts)
 	}, timeout*3, interval).Should(Equal(3))
 }
+
+func Test_000320_default_vol_mount_test(t *testing.T) {
+	Spec("This spec describes the runner pod with no additional vol mounts.")
+	It("should be reconciled, planned successfully, and stopped to wait for a manual approve.")
+
+	const (
+		sourceName    = "default-vol-mount-test-source"
+		terraformName = "default-vol-mount-test-terraform"
+	)
+	g := NewWithT(t)
+	ctx := context.Background()
+
+	Given("a GitRepository")
+	By("defining a new GitRepository resource.")
+	updatedTime := time.Now()
+	testRepo := sourcev1.GitRepository{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      sourceName,
+			Namespace: "flux-system",
+		},
+		Spec: sourcev1.GitRepositorySpec{
+			URL: "https://github.com/openshift-fluxv2-poc/podinfo",
+			Reference: &sourcev1.GitRepositoryRef{
+				Branch: "master",
+			},
+			Interval:          metav1.Duration{Duration: time.Second * 30},
+			GitImplementation: "go-git",
+		},
+	}
+
+	By("creating the GitRepository resource in the cluster.")
+	It("should be created successfully.")
+	g.Expect(k8sClient.Create(ctx, &testRepo)).Should(Succeed())
+	defer func() { g.Expect(k8sClient.Delete(ctx, &testRepo)).Should(Succeed()) }()
+
+	Given("the GitRepository's reconciled status.")
+	By("setting the GitRepository's status, with the downloadable BLOB's URL, and the correct checksum.")
+	testRepo.Status = sourcev1.GitRepositoryStatus{
+		ObservedGeneration: int64(1),
+		Conditions: []metav1.Condition{
+			{
+				Type:               "Ready",
+				Status:             metav1.ConditionTrue,
+				LastTransitionTime: metav1.Time{Time: updatedTime},
+				Reason:             "GitOperationSucceed",
+				Message:            "Fetched revision: master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			},
+		},
+		URL: server.URL() + "/file.tar.gz",
+		Artifact: &sourcev1.Artifact{
+			Path:           "gitrepository/flux-system/test-tf-controller/b8e362c206e3d0cbb7ed22ced771a0056455a2fb.tar.gz",
+			URL:            server.URL() + "/file.tar.gz",
+			Revision:       "master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			Checksum:       "80ddfd18eb96f7d31cadc1a8a5171c6e2d95df3f6c23b0ed9cd8dddf6dba1406", // must be the real checksum value
+			LastUpdateTime: metav1.Time{Time: updatedTime},
+		},
+	}
+	It("should be updated successfully.")
+	g.Expect(k8sClient.Status().Update(ctx, &testRepo)).Should(Succeed())
+
+	By("checking that the status and its URL gets reconciled")
+	gitRepoKey := types.NamespacedName{Namespace: "flux-system", Name: sourceName}
+	createdRepo := sourcev1.GitRepository{}
+	g.Expect(k8sClient.Get(ctx, gitRepoKey, &createdRepo)).Should(Succeed())
+
+	Given("a Terraform resource with manual approve, attached to the given GitRepository")
+	By("creating a new TF resource and attaching to the repo via `sourceRef`, with no .spec.approvePlan specified.")
+	helloWorldTF := infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      terraformName,
+			Namespace: "flux-system",
+		},
+		Spec: infrav1.TerraformSpec{
+			Path:     "./terraform-hello-world-example",
+			Interval: metav1.Duration{Duration: 3 * time.Second},
+			SourceRef: infrav1.CrossNamespaceSourceReference{
+				Kind:      "GitRepository",
+				Name:      sourceName,
+				Namespace: "flux-system",
+			},
+			RunnerPodTemplate: infrav1.RunnerPodTemplate{
+				Spec: infrav1.RunnerPodSpec{},
+			},
+		},
+	}
+	It("should be created and attached successfully.")
+	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
+	defer func() { g.Expect(k8sClient.Delete(ctx, &helloWorldTF)).Should(Succeed()) }()
+
+	By("checking that the TF resource existed inside the cluster.")
+	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
+	createdHelloWorldTF := infrav1.Terraform{}
+	g.Eventually(func() bool {
+		err := k8sClient.Get(ctx, helloWorldTFKey, &createdHelloWorldTF)
+		if err != nil {
+			return false
+		}
+		return true
+	}, timeout, interval).Should(BeTrue())
+
+	It("should run the pod")
+	By("checking that there is a vol mount defined")
+	runnerPod := corev1.Pod{}
+	g.Eventually(func() int {
+		err := k8sClient.Get(ctx, types.NamespacedName{Namespace: "flux-system", Name: fmt.Sprintf("%s-tf-runner", terraformName)}, &createdHelloWorldTF)
+		if err != nil {
+			return -1
+		}
+		return len(runnerPod.Spec.Containers[0].VolumeMounts)
+	}, timeout*3, interval).Should(Equal(2))
+}
