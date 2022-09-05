d71830ff06bfcf04f9233357fea01072ece67913
Steven Fraser <steve.fraser@weave.works>
2022-09-05 10:43:14 -0700

    finish test coverge for vols and vol mounts

--- controllers/tc000320_vol_and_vol_mounts_test.go
+++ controllers/tc000320_vol_and_vol_mounts_test.go
@@ -116,7 +116,7 @@ spec:
 	}, timeout, interval).Should(BeTrue())
 
 	It("should run the pod")
-	By("checking that there is a node selector defined")
+	By("checking that there is a default vols defined")
 	runnerPod := corev1.PodSpec{}
 	g.Eventually(func() []corev1.Volume {
 		runnerPod = reconciler.runnerPodSpec(createdHelloWorldTF, "tlsSecret")
@@ -236,7 +236,7 @@ spec:
 	}, timeout, interval).Should(BeTrue())
 
 	It("should run the pod")
-	By("checking that there is a node selector defined")
+	By("checking that there is a default vol mounts defined")
 	runnerPod := corev1.PodSpec{}
 	g.Eventually(func() []corev1.VolumeMount {
 		runnerPod = reconciler.runnerPodSpec(createdHelloWorldTF, "tlsSecret")
@@ -335,9 +335,9 @@ spec:
     namespace: flux-system
   runnerPodTemplate:
     spec:
-      volumes:
-	  - name: test-vol
-		emptyDir: {}
+     volumes:
+     - name: test-vol
+       emptyDir: {}
 `, terraformName, sourceName)), runnerServer.Scheme)
 	g.Expect(err).ToNot(HaveOccurred())
 
@@ -357,7 +357,7 @@ spec:
 	}, timeout, interval).Should(BeTrue())
 
 	It("should run the pod")
-	By("checking that there is a node selector defined")
+	By("checking that there is additional vols defined")
 	runnerPod := corev1.PodSpec{}
 	g.Eventually(func() []corev1.Volume {
 		runnerPod = reconciler.runnerPodSpec(createdHelloWorldTF, "tlsSecret")
@@ -374,6 +374,137 @@ spec:
 			VolumeSource: corev1.VolumeSource{
 				EmptyDir: &corev1.EmptyDirVolumeSource{},
 			},
+		},
+		{
+			Name: "test-vol",
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{},
+			},
+		},
+	}))
+}
+
+func Test_000320_volume_mounts_test(t *testing.T) {
+	Spec("This spec describes the runner pod when additional Volume Mounts are defined.")
+	It("should be reconciled, planned successfully, and stopped to wait for a manual approve.")
+
+	const (
+		sourceName    = "vol-mounts-test-source"
+		terraformName = "vol-mounts-test-terraform"
+	)
+	g := NewWithT(t)
+	ctx := context.Background()
+
+	Given("a GitRepository")
+	By("defining a new GitRepository resource.")
+	updatedTime := time.Now()
+	testRepo := sourcev1.GitRepository{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      sourceName,
+			Namespace: "flux-system",
+		},
+		Spec: sourcev1.GitRepositorySpec{
+			URL: "https://github.com/openshift-fluxv2-poc/podinfo",
+			Reference: &sourcev1.GitRepositoryRef{
+				Branch: "master",
+			},
+			Interval:          metav1.Duration{Duration: time.Second * 30},
+			GitImplementation: "go-git",
+		},
+	}
+
+	By("creating the GitRepository resource in the cluster.")
+	It("should be created successfully.")
+	g.Expect(k8sClient.Create(ctx, &testRepo)).Should(Succeed())
+	defer func() { g.Expect(k8sClient.Delete(ctx, &testRepo)).Should(Succeed()) }()
+
+	Given("the GitRepository's reconciled status.")
+	By("setting the GitRepository's status, with the downloadable BLOB's URL, and the correct checksum.")
+	testRepo.Status = sourcev1.GitRepositoryStatus{
+		ObservedGeneration: int64(1),
+		Conditions: []metav1.Condition{
+			{
+				Type:               "Ready",
+				Status:             metav1.ConditionTrue,
+				LastTransitionTime: metav1.Time{Time: updatedTime},
+				Reason:             "GitOperationSucceed",
+				Message:            "Fetched revision: master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			},
+		},
+		URL: server.URL() + "/file.tar.gz",
+		Artifact: &sourcev1.Artifact{
+			Path:           "gitrepository/flux-system/test-tf-controller/b8e362c206e3d0cbb7ed22ced771a0056455a2fb.tar.gz",
+			URL:            server.URL() + "/file.tar.gz",
+			Revision:       "master/b8e362c206e3d0cbb7ed22ced771a0056455a2fb",
+			Checksum:       "80ddfd18eb96f7d31cadc1a8a5171c6e2d95df3f6c23b0ed9cd8dddf6dba1406", // must be the real checksum value
+			LastUpdateTime: metav1.Time{Time: updatedTime},
+		},
+	}
+	It("should be updated successfully.")
+	g.Expect(k8sClient.Status().Update(ctx, &testRepo)).Should(Succeed())
+
+	By("checking that the status and its URL gets reconciled")
+	gitRepoKey := types.NamespacedName{Namespace: "flux-system", Name: sourceName}
+	createdRepo := sourcev1.GitRepository{}
+	g.Expect(k8sClient.Get(ctx, gitRepoKey, &createdRepo)).Should(Succeed())
+
+	Given("a Terraform resource with manual approve, attached to the given GitRepository")
+	By("creating a new TF resource and attaching to the repo via `sourceRef`, with no .spec.approvePlan specified.")
+	helloWorldTF := infrav1.Terraform{}
+	err := helloWorldTF.FromBytes([]byte(fmt.Sprintf(`
+apiVersion: infra.contrib.fluxcd.io/v1alpha1
+kind: Terraform
+metadata:
+  name: %s
+  namespace: flux-system
+spec:
+  path: ./terraform-hello-world-example
+  interval: 10s
+  sourceRef:
+    kind: GitRepository
+    name: %s
+    namespace: flux-system
+  runnerPodTemplate:
+    spec:
+     volumeMounts:
+     - mountPath: /etc/test
+       name: test-vol
+`, terraformName, sourceName)), runnerServer.Scheme)
+	g.Expect(err).ToNot(HaveOccurred())
+
+	It("should be created and attached successfully.")
+	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
+	defer func() { g.Expect(k8sClient.Delete(ctx, &helloWorldTF)).Should(Succeed()) }()
+
+	By("checking that the TF resource existed inside the cluster.")
+	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
+	createdHelloWorldTF := infrav1.Terraform{}
+	g.Eventually(func() bool {
+		err := k8sClient.Get(ctx, helloWorldTFKey, &createdHelloWorldTF)
+		if err != nil {
+			return false
+		}
+		return true
+	}, timeout, interval).Should(BeTrue())
+
+	It("should run the pod")
+	By("checking that there is vol mounts defined")
+	runnerPod := corev1.PodSpec{}
+	g.Eventually(func() []corev1.VolumeMount {
+		runnerPod = reconciler.runnerPodSpec(createdHelloWorldTF, "tlsSecret")
+		return runnerPod.Containers[0].VolumeMounts
+	}, timeout, interval).Should(Equal([]corev1.VolumeMount{
+		{
+			Name:      "temp",
+			MountPath: "/tmp",
+		},
+		{
+			Name:      "home",
+			MountPath: "/home/runner",
+		},
+		{
+			Name:      "test-vol",
+			MountPath: "/etc/test",
 		},
 	}))
 }
