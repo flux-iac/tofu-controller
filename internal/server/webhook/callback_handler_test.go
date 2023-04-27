f637a5014c6835bcd81de3b391b6ebcc20f9cd5a
Balazs Nadasdi <balazs@weave.works>
2023-04-27 10:01:05 +0200

    fix: address comments
    I don't like gomega as it's much easier to write messy code and tests,
    and it allows to write complex tests with seemingly simpler way, but at
    the end the output is completely useless on error and needs a lot more
    time to debug. Of course, not in all cases, but with gomega it's much
    easier to get these. The TDD part of gomega is not something I would
    really want to use anywhere, we are not using that, and from that point,
    it does not really have much more benefits compared to much simpler
    and lighter alternatives like 'stretchr/testify'.
    
    The next question can be why I don't use the "gomega" prefix and not
    import it as a dot-import ('.'). The reason is simple, I don't like
    dot-imports, I'm not going to use it on my own, and I'll always warn
    people to use it. If I use a package, then all functions belong to that
    package and NOT in the scope on my code. In my eyes, dot-import was a
    mistake by the Go developers.
    
    Signed-off-by: Balazs Nadasdi <balazs@weave.works>

--- internal/server/webhook/callback_handler_test.go
+++ internal/server/webhook/callback_handler_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 
 	"github.com/go-logr/logr"
-	"github.com/stretchr/testify/assert"
+	"github.com/onsi/gomega"
 	"github.com/tonglil/buflogr"
 	"github.com/weaveworks/tf-controller/internal/server/webhook"
 )
@@ -19,6 +19,8 @@ import (
 const webhookSecret = "33713b74ae24b9be98a5c0f1dc4c864189294228"
 
 func TestCallbackHandler_InvalidRequestMethod(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
 	handler := webhook.NewCallbackHandler(logr.Discard())
 	testCases := []string{
 		http.MethodGet,
@@ -28,12 +30,12 @@ func TestCallbackHandler_InvalidRequestMethod(t *testing.T) {
 	}
 
 	for _, method := range testCases {
-		t.Run(method, func(t *testing.T) {
+		t.Run(method, func(_ *testing.T) {
 			request := httptest.NewRequest(method, "/callback?provider=github", nil)
 			responseRecorder := httptest.NewRecorder()
 
 			handler.ServeHTTP(responseRecorder, request)
-			assert.Equal(t, http.StatusMethodNotAllowed, responseRecorder.Code)
+			g.Expect(responseRecorder.Code).To(gomega.Equal(http.StatusMethodNotAllowed))
 		})
 	}
 }
@@ -59,24 +61,26 @@ func TestCallbackHandler_InvalidProvider(t *testing.T) {
 		},
 	}
 
-	var buf bytes.Buffer
-	var log logr.Logger = buflogr.NewWithBuffer(&buf)
+	g := gomega.NewGomegaWithT(t)
+
+	var (
+		buf bytes.Buffer
+		log logr.Logger = buflogr.NewWithBuffer(&buf)
+	)
 
 	handler := webhook.NewCallbackHandler(log)
 	body, header, err := readRequestFixture("pull_request_event")
-	if err != nil {
-		t.Fatal(err)
-	}
+	g.Expect(err).NotTo(gomega.HaveOccurred())
 
 	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
+		t.Run(testCase.name, func(_ *testing.T) {
 			request := httptest.NewRequest(http.MethodPost, testCase.url, bytes.NewReader(body))
 			request.Header = header
 			responseRecorder := httptest.NewRecorder()
 
 			handler.ServeHTTP(responseRecorder, request)
-			assert.Equal(t, testCase.code, responseRecorder.Code)
-			assert.Contains(t, buf.String(), testCase.logContains)
+			g.Expect(responseRecorder.Code).To(gomega.Equal(testCase.code))
+			g.Expect(buf.String()).To(gomega.ContainSubstring(testCase.logContains))
 		})
 	}
 }
@@ -84,14 +88,16 @@ func TestCallbackHandler_InvalidProvider(t *testing.T) {
 func TestCallbackHandler_MissingHMAC(t *testing.T) {
 	os.Setenv("WEBHOOK_HMAC_KEY", webhookSecret)
 
-	var buf bytes.Buffer
-	var log logr.Logger = buflogr.NewWithBuffer(&buf)
+	g := gomega.NewGomegaWithT(t)
+
+	var (
+		buf bytes.Buffer
+		log logr.Logger = buflogr.NewWithBuffer(&buf)
+	)
 
 	handler := webhook.NewCallbackHandler(log)
 	body, header, err := readRequestFixture("pull_request_event")
-	if err != nil {
-		t.Fatal(err)
-	}
+	g.Expect(err).NotTo(gomega.HaveOccurred())
 
 	header.Del(webhook.HubSignatureSHA256Header)
 	header.Del(webhook.HubSignatureSHA1Header)
@@ -101,71 +107,77 @@ func TestCallbackHandler_MissingHMAC(t *testing.T) {
 	responseRecorder := httptest.NewRecorder()
 
 	handler.ServeHTTP(responseRecorder, request)
-	assert.Equal(t, http.StatusUnauthorized, responseRecorder.Code)
-	assert.Contains(t, buf.String(), "missing hmac signature")
+	g.Expect(responseRecorder.Code).To(gomega.Equal(http.StatusUnauthorized))
+	g.Expect(buf.String()).To(gomega.ContainSubstring("missing hmac signature"))
 }
 
 func TestCallbackHandler_InvalidHMAC(t *testing.T) {
 	os.Setenv("WEBHOOK_HMAC_KEY", "invalid-key")
 
-	var buf bytes.Buffer
-	var log logr.Logger = buflogr.NewWithBuffer(&buf)
+	g := gomega.NewGomegaWithT(t)
+
+	var (
+		buf bytes.Buffer
+		log logr.Logger = buflogr.NewWithBuffer(&buf)
+	)
 
 	handler := webhook.NewCallbackHandler(log)
 	body, header, err := readRequestFixture("pull_request_event")
-	if err != nil {
-		t.Fatal(err)
-	}
+	g.Expect(err).NotTo(gomega.HaveOccurred())
 
 	request := httptest.NewRequest(http.MethodPost, "/callback?provider=github", bytes.NewReader(body))
 	request.Header = header
 	responseRecorder := httptest.NewRecorder()
 
 	handler.ServeHTTP(responseRecorder, request)
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Contains(t, buf.String(), "invalid webhook signature parsing webhook request")
+	g.Expect(responseRecorder.Code).To(gomega.Equal(http.StatusBadRequest))
+	g.Expect(buf.String()).To(gomega.ContainSubstring("invalid webhook signature parsing webhook request"))
 }
 
 func TestCallbackHandler_PullRequestEvent(t *testing.T) {
 	os.Setenv("WEBHOOK_HMAC_KEY", webhookSecret)
 
-	var buf bytes.Buffer
-	var log logr.Logger = buflogr.NewWithBuffer(&buf)
+	g := gomega.NewGomegaWithT(t)
+
+	var (
+		buf bytes.Buffer
+		log logr.Logger = buflogr.NewWithBuffer(&buf)
+	)
 
 	handler := webhook.NewCallbackHandler(log)
 	body, header, err := readRequestFixture("pull_request_event")
-	if err != nil {
-		t.Fatal(err)
-	}
+	g.Expect(err).NotTo(gomega.HaveOccurred())
 
 	request := httptest.NewRequest(http.MethodPost, "/callback?provider=github", bytes.NewReader(body))
 	request.Header = header
 	responseRecorder := httptest.NewRecorder()
 
 	handler.ServeHTTP(responseRecorder, request)
-	assert.Equal(t, http.StatusAccepted, responseRecorder.Code)
-	assert.Contains(t, buf.String(), "incoming hmac request is valid kind pull_request")
+	g.Expect(responseRecorder.Code).To(gomega.Equal(http.StatusAccepted))
+	g.Expect(buf.String()).To(gomega.ContainSubstring("incoming hmac request is valid kind pull_request"))
 }
 
 func TestCallbackHandler_CommentEvent(t *testing.T) {
 	os.Setenv("WEBHOOK_HMAC_KEY", webhookSecret)
 
-	var buf bytes.Buffer
-	var log logr.Logger = buflogr.NewWithBuffer(&buf)
+	g := gomega.NewGomegaWithT(t)
+
+	var (
+		buf bytes.Buffer
+		log logr.Logger = buflogr.NewWithBuffer(&buf)
+	)
 
 	handler := webhook.NewCallbackHandler(log)
 	body, header, err := readRequestFixture("comment_event")
-	if err != nil {
-		t.Fatal(err)
-	}
+	g.Expect(err).NotTo(gomega.HaveOccurred())
 
 	request := httptest.NewRequest(http.MethodPost, "/callback?provider=github", bytes.NewReader(body))
 	request.Header = header
 	responseRecorder := httptest.NewRecorder()
 
 	handler.ServeHTTP(responseRecorder, request)
-	assert.Equal(t, http.StatusAccepted, responseRecorder.Code)
-	assert.Contains(t, buf.String(), "incoming hmac request is valid kind issue_comment")
+	g.Expect(responseRecorder.Code).To(gomega.Equal(http.StatusAccepted))
+	g.Expect(buf.String()).To(gomega.ContainSubstring("incoming hmac request is valid kind issue_comment"))
 }
 
 func readFixture(filename string) ([]byte, error) {
