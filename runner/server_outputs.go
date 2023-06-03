da26a402cc093178666a5fe58829420a915f3f0e
Chanwit Kaewkasi <chanwit@gmail.com>
2023-06-03 23:44:05 +0700

    fix output
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- runner/server_outputs.go
+++ runner/server_outputs.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"reflect"
 
-	infrav1 "github.com/weaveworks/tf-controller/api/v1alpha2"
+	infrav1 "github.com/weaveworks/tf-controller/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
