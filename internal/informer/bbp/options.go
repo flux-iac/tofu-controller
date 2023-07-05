c4e1b54f88110e0d05b9b58f109aa6bd4e83f5dc
Luiz Filho <luiz.filho@weave.works>
2023-07-05 17:58:57 -0300

    Tweaking minor things

--- internal/informer/bbp/options.go
+++ internal/informer/bbp/options.go
@@ -1,10 +1,7 @@
 package bbp
 
 import (
-	"fmt"
-
 	"github.com/go-logr/logr"
-	"github.com/weaveworks/tf-controller/internal/config"
 	"github.com/weaveworks/tf-controller/internal/git/provider"
 	"k8s.io/client-go/tools/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -21,22 +18,6 @@ func WithLogger(log logr.Logger) Option {
 func WithClusterClient(clusterClient client.Client) Option {
 	return func(i *Informer) error {
 		i.client = clusterClient
-
-		return nil
-	}
-}
-
-func WithConfigMapRef(configMapName string) Option {
-	return func(i *Informer) error {
-		key, err := config.ObjectKeyFromName(configMapName)
-		if err != nil {
-			return fmt.Errorf("failed getting object key from config map name: %w", err)
-		}
-
-		i.configMapRef = client.ObjectKey{
-			Namespace: key.Namespace,
-			Name:      key.Name,
-		}
 
 		return nil
 	}
