88f9d21717b9459c15befe852a2fb2a0a741bc9a
Balazs Nadasdi <balazs@weave.works>
2023-08-31 18:27:45 +0200

    better waitResourceToBeDelete
    I tried to create a `Cleaner` struct like:
    
    ```go
    type Cleaner struct {
      resources []client.Object
    }
    
    func (c *Cleaner) AddResource(client.Object) {}
    func (c *Cleaner) Cleanup(gomega)
    ```
    
    so we could use it like
    
    ```go
    cleaner := NewCleaner()
    defer cleaner.Cleanup()
    
    // ...
    
    cleaner.AddResource(&tfresource)
    ```
    
    But for some reasons, no matter what I tried it stuck on the
    `client.Get` in `Eventually`.
    
    I don't like global variables, but using it in the
    `waitResourceToBeDelete` instead of an extra argument makes it cleaner
    to use in tests.
    
    Signed-off-by: Balazs Nadasdi <balazs@weave.works>

--- controllers/cleaner_test.go
+++ controllers/cleaner_test.go
@@ -1,37 +1,19 @@
 package controllers
 
 import (
-	"sync"
+	"context"
 
 	"github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type resourceToBeDeleted struct {
-	Namespace string
-	Name      string
-	Object    client.Object
-}
+func waitResourceToBeDelete(g gomega.Gomega, resource client.Object) {
+	ctx := context.Background()
+	key := types.NamespacedName{Namespace: resource.GetNamespace(), Name: resource.GetName()}
 
-func waitResourceToBeDelete(g gomega.Gomega, resource resourceToBeDeleted) {
+	g.Expect(k8sClient.Delete(ctx, resource)).Should(gomega.Succeed())
 	g.Eventually(func() error {
-		key := types.NamespacedName{Namespace: resource.Namespace, Name: resource.Name}
-
-		return k8sClient.Get(ctx, key, resource.Object)
+		return k8sClient.Get(ctx, key, resource)
 	}, timeout, interval).ShouldNot(gomega.Succeed())
-}
-
-func waitResourcesToBeDelete(g gomega.Gomega, resources []resourceToBeDeleted) {
-	var wg sync.WaitGroup
-
-	for _, resource := range resources {
-		wg.Add(1)
-		go func(resource resourceToBeDeleted) {
-			waitResourceToBeDelete(g, resource)
-			wg.Done()
-		}(resource)
-	}
-
-	wg.Wait()
 }
