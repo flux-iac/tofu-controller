9907b5ca2ee42d4147da5adebd23fb4f394a8bf4
Luiz Filho <luiz.filho@weave.works>
2023-09-07 12:47:58 -0300

    Waiting resources deletion to avoid test flakiness

--- controllers/tc000080_varsfrom_configmap_and_controlled_outputs_test.go
+++ controllers/tc000080_varsfrom_configmap_and_controlled_outputs_test.go
@@ -40,7 +40,7 @@ func Test_000080_varsfrom_configmap_and_controlled_outputs_test(t *testing.T) {
 		},
 	}
 	g.Expect(k8sClient.Create(ctx, &testRepo)).Should(Succeed())
-	defer func() { g.Expect(k8sClient.Delete(ctx, &testRepo)).Should(Succeed()) }()
+	defer waitResourceToBeDelete(g, &testRepo)
 
 	By("setting the git repo status object, the URL, and the correct checksum")
 	testRepo.Status = sourcev1.GitRepositoryStatus{
@@ -81,7 +81,7 @@ func Test_000080_varsfrom_configmap_and_controlled_outputs_test(t *testing.T) {
 		},
 	}
 	g.Expect(k8sClient.Create(ctx, &myVars)).Should(Succeed())
-	defer func() { g.Expect(k8sClient.Delete(ctx, &myVars)).Should(Succeed()) }()
+	defer waitResourceToBeDelete(g, &myVars)
 
 	By("creating a new TF and attaching to the repo")
 	helloWorldTF := infrav1.Terraform{
@@ -114,7 +114,7 @@ func Test_000080_varsfrom_configmap_and_controlled_outputs_test(t *testing.T) {
 		},
 	}
 	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
-	defer func() { g.Expect(k8sClient.Delete(ctx, &helloWorldTF)).Should(Succeed()) }()
+	defer waitResourceToBeDelete(g, &helloWorldTF)
 
 	By("checking that the hello world TF got created")
 	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
