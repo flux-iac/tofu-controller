7c316c5238153103c9e695de3718fc6155756b81
Balazs Nadasdi <balazs@weave.works>
2023-09-07 16:29:54 +0200

    fix: controllers/tc00004x tests
    They passed constistently running them 3 times in a row without break, so no
    main suit cleanup.
    
    ```
    ❯ go test ./controllers -parallel 1 -count=3 -test.run Test_00004
    ok      github.com/weaveworks/tf-controller/controllers 22.412s
    ```
    
    Fixes #904
    
    References:
    * https://github.com/weaveworks/tf-controller/issues/904
    
    Signed-off-by: Balazs Nadasdi <balazs@weave.works>

--- controllers/tc000043_controlled_outputs_should_be_reconciled_test.go
+++ controllers/tc000043_controlled_outputs_should_be_reconciled_test.go
@@ -1,5 +1,3 @@
-//go:build flaky
-
 package controllers
 
 import (
@@ -49,7 +47,7 @@ func Test_000043_controlled_outputs_should_be_reconciled_test(t *testing.T) {
 	By("creating the GitRepository resource in the cluster.")
 	It("should be created successfully.")
 	g.Expect(k8sClient.Create(ctx, &testRepo)).Should(Succeed())
-	defer func() { g.Expect(k8sClient.Delete(ctx, &testRepo)).Should(Succeed()) }()
+	defer waitResourceToBeDelete(g, &testRepo)
 
 	Given("the GitRepository's reconciled status")
 	By("setting the GitRepository's status, with the downloadable BLOB's URL, and the correct checksum.")
@@ -108,7 +106,7 @@ func Test_000043_controlled_outputs_should_be_reconciled_test(t *testing.T) {
 	}
 	It("should be created and attached successfully.")
 	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
-	defer func() { g.Expect(k8sClient.Delete(ctx, &helloWorldTF)).Should(Succeed()) }()
+	defer waitResourceToBeDelete(g, &helloWorldTF)
 
 	By("checking that the TF resource existed inside the cluster.")
 	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
