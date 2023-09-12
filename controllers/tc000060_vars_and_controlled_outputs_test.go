75956ba7fd2c220d8f0406bec6cdf6221eb89ae6
Luiz Filho <luiz.filho@weave.works>
2023-09-12 13:18:19 -0300

    Wait resource deletion

--- controllers/tc000060_vars_and_controlled_outputs_test.go
+++ controllers/tc000060_vars_and_controlled_outputs_test.go
@@ -1,5 +1,3 @@
-//go:build flaky
-
 package controllers
 
 import (
@@ -42,7 +40,7 @@ func Test_000060_vars_and_controlled_outputs_test(t *testing.T) {
 		},
 	}
 	g.Expect(k8sClient.Create(ctx, &testRepo)).Should(Succeed())
-	defer func() { g.Expect(k8sClient.Delete(ctx, &testRepo)).Should(Succeed()) }()
+	defer waitResourceToBeDelete(g, &testRepo)
 
 	By("setting the git repo status object, the URL, and the correct checksum")
 	testRepo.Status = sourcev1.GitRepositoryStatus{
@@ -102,7 +100,7 @@ func Test_000060_vars_and_controlled_outputs_test(t *testing.T) {
 		},
 	}
 	g.Expect(k8sClient.Create(ctx, &helloWorldTF)).Should(Succeed())
-	defer func() { g.Expect(k8sClient.Delete(ctx, &helloWorldTF)).Should(Succeed()) }()
+	defer waitResourceToBeDelete(g, &helloWorldTF)
 
 	By("checking that the hello world TF got created")
 	helloWorldTFKey := types.NamespacedName{Namespace: "flux-system", Name: terraformName}
