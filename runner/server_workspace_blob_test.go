77e0b3977a7572aa9fa20ea85b6d9dd90781adbc
Balazs Nadasdi <balazs@weave.works>
2023-12-07 13:05:35 +0100

    feat: Implement BLOB encryption within the tf-runner
    As a starter, we need a ServiceAccountToken Secret. With older versions of
    Kubernetes, a default one was created automatically, but recent versions
    doesn't have this functionality.
    
    ```
    apiVersion: v1
    kind: Secret
    metadata:
      name: tf-runner-encryption
      namespace: terraform
      annotations:
        kubernetes.io/service-account.name: tf-runner
    type: kubernetes.io/service-account-token
    ```
    
    When encrypting the BLOB content, we use the first 32 bytes of the
    generated token as AES256 key with a nonce using Galois Counter Mode.
    
    The full encryption is part of the `CreateWorkspaceBlob` function for
    now. It can be extracted, but until we need somewhere else it can live
    there.
    
    There is a chance we have to reconsider how it works when we approach
    the key rotation with #1161, because with this approach if the token is
    replaced, we have zero clue what was the previous one, therefore we
    can't decrypt existing content. It can be a design choice tho (discard
    all cache when key rotation happens).
    
    Closes #1121
    
    References:
    - https://github.com/weaveworks/tf-controller/issues/1121
    - https://github.com/weaveworks/tf-controller/issues/1161
    
    Signed-off-by: Balazs Nadasdi <balazs@weave.works>

--- runner/server_workspace_blob_test.go
+++ runner/server_workspace_blob_test.go
@@ -2,6 +2,9 @@ package runner_test
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -9,15 +12,35 @@ import (
 	"github.com/fluxcd/pkg/untar"
 	. "github.com/onsi/gomega"
 	"github.com/weaveworks/tf-controller/runner"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+const encryptionToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImJVM0xaLXN3OUJYRFNEejF3THl2X3VvSGxoOWlHdXhYNHdTdV9Vc2w4QjAifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJ0ZXJyYWZvcm0iLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoidGYtcnVubmVyLWVuY3J5cHRpb24iLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoidGYtcnVubmVyIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiYjJiMTc4MjMtMWI5Yi00YzEzLThhMDctNmE0OThmNjUwYjM3Iiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OnRlcnJhZm9ybTp0Zi1ydW5uZXIifQ.s2T3_Yd-PNF0dJO-7sP_yKbohCP-GTWrHPACUQs0nQrD3hMjZTXm-CgQdtzuKPN0fPHp_GJ8iDpWrqMRcZSqHKVSXscfCI7-QnGjqwSvt-8KBMGE7J29tFgFy6-K6uvP_kYAaA5i4bDWPXHytLmOHJj7GL_D4-0XXVB3EmCfzwREl19FdjZnmEf8lB4gJ7aOZQnW6FzJHcdzo3bUwh-S0zrjGkGsBbrBBu5hyhCKoyZP1ufn8X9NQfkdtC29rEYgI_6o2gbQrGZRdIujAVgh3HJaU2bodV4sGAdgqsMDHEeoyGzp4LSlSrR2kAYJJznF0bMFY18eojbNXnmoIpkMEQ"
 
 func TestCreateWorkspaceBlob(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	tempDir := t.TempDir()
 
+	encKeySecret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "tf-runner.cache-encryption",
+			Namespace: "flux-system",
+			Annotations: map[string]string{
+				"kubernetes.io/service-account.name": "tf-runner",
+			},
+		},
+		Data: map[string][]byte{
+			"token": []byte(encryptionToken),
+		},
+		Type: v1.SecretTypeServiceAccountToken,
+	}
+	err := k8sClient.Create(ctx, encKeySecret)
+	g.Expect(err).To(BeNil())
+
 	terraformDir := filepath.Join(tempDir, ".terraform")
-	err := os.Mkdir(terraformDir, 0755)
+	err = os.Mkdir(terraformDir, 0755)
 	g.Expect(err).To(BeNil())
 
 	filePath := filepath.Join(terraformDir, "random.txt")
@@ -25,15 +48,30 @@ func TestCreateWorkspaceBlob(t *testing.T) {
 	err = os.WriteFile(filePath, randomContent, 0644)
 	g.Expect(err).To(BeNil())
 
-	resp, err := runnerClient.CreateWorkspaceBlob(ctx, &runner.CreateWorkspaceBlobRequest{TfInstance: "test", WorkingDir: tempDir})
+	resp, err := runnerClient.CreateWorkspaceBlob(ctx, &runner.CreateWorkspaceBlobRequest{TfInstance: "test", WorkingDir: tempDir, Namespace: "flux-system"})
 	g.Expect(err).To(BeNil())
 
 	g.Expect(resp.GetSha256Checksum()).ToNot(BeEmpty())
 
-	blobReader := bytes.NewReader(resp.Blob)
+	token := encKeySecret.Data["token"]
+	key := token[:runner.EncryptionKeyLength]
+
+	// Decrypting content.
+	aesCipher, err := aes.NewCipher(key)
+	g.Expect(err).To(BeNil())
+	gcm, err := cipher.NewGCM(aesCipher)
+	g.Expect(err).To(BeNil())
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := resp.Blob[:nonceSize], resp.Blob[nonceSize:]
+	g.Expect(err).To(BeNil())
+	blob, err := gcm.Open(nil, nonce, ciphertext, nil)
+
+	blobReader := bytes.NewReader(blob)
 
 	outputTempDir := t.TempDir()
-	untar.Untar(blobReader, outputTempDir)
+	sum, err := untar.Untar(blobReader, outputTempDir)
+	g.Expect(err).To(BeNil())
+	fmt.Println(sum)
 
 	outputFilePath := filepath.Join(outputTempDir, ".terraform", "random.txt")
 	outputContent, err := os.ReadFile(outputFilePath)
