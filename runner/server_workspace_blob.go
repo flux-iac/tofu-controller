164153556fb231474a959f0c252f657c9cc8e6aa
Balazs Nadasdi <balazs@weave.works>
2023-12-07 13:05:35 +0100

    feat: Implement BLOB encryption within the tf-runner
    As a starter, we need a ServiceAccountToken Secret. With older versions of
    Kubernetes, a default one was created automatically, but recent versions
    doesn't have this functionality.
    
    ```
    apiVersion: v1
    kind: Secret
    metadata:
      name: tf-runner-encryption
      namespace: terraform
      annotations:
        kubernetes.io/service-account.name: tf-runner
    type: kubernetes.io/service-account-token
    ```
    
    When encrypting the BLOB content, we use the first 32 bytes of the
    generated token as AES256 key with a nonce using Galois Counter Mode.
    
    The full encryption is part of the `CreateWorkspaceBlob` function for
    now. It can be extracted, but until we need somewhere else it can live
    there.
    
    There is a chance we have to reconsider how it works when we approach
    the key rotation with #1161, because with this approach if the token is
    replaced, we have zero clue what was the previous one, therefore we
    can't decrypt existing content. It can be a design choice tho (discard
    all cache when key rotation happens).
    
    Closes #1121
    
    References:
    - https://github.com/weaveworks/tf-controller/issues/1121
    - https://github.com/weaveworks/tf-controller/issues/1161
    
    Signed-off-by: Balazs Nadasdi <balazs@weave.works>

--- runner/server_workspace_blob.go
+++ runner/server_workspace_blob.go
@@ -2,14 +2,21 @@ package runner
 
 import (
 	context "context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/weaveworks/tf-controller/internal/storage"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
+
+const EncryptionKeyLength = 32
 
 // CreateWorkspaceBlob archives and compresses using tar and gzip the .terraform directory and returns the tarball as a byte array
 func (r *TerraformRunnerServer) CreateWorkspaceBlob(ctx context.Context, req *CreateWorkspaceBlobRequest) (*CreateWorkspaceBlobReply, error) {
@@ -20,6 +27,7 @@ func (r *TerraformRunnerServer) CreateWorkspaceBlob(ctx context.Context, req *Cr
 		return nil, err
 	}
 
+	log.Info("archiving workspace directory", "dir", req.WorkingDir)
 	archivePath, err := storage.ArchiveDir(filepath.Join(req.WorkingDir, ".terraform"))
 	if err != nil {
 		log.Error(err, "unable to archive .terraform directory")
@@ -33,14 +41,48 @@ func (r *TerraformRunnerServer) CreateWorkspaceBlob(ctx context.Context, req *Cr
 		return nil, err
 	}
 
+	secretName := "tf-runner.cache-encryption"
+	encryptionSecretKey := types.NamespacedName{Name: secretName, Namespace: req.Namespace}
+	var encryptionSecret v1.Secret
+
+	log.Info("fetching secret key", "key", encryptionSecretKey)
+	if err := r.Client.Get(ctx, encryptionSecretKey, &encryptionSecret); err != nil {
+		return nil, err
+	}
+
+	// 256 bit AES encryption with Galois Counter Mode.
+	log.Info("encrypting content")
+	token := encryptionSecret.Data["token"]
+	key := token[:EncryptionKeyLength]
+
+	aesCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(aesCipher)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = rand.Read(nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	out := gcm.Seal(nonce, nonce, blob, nil)
+
+	// SHA256 checksum so we can verify if the saved content is not corrupted.
+	log.Info("generating sha256 checksum")
 	sha := sha256.New()
-	if _, err := sha.Write(blob); err != nil {
-		return &CreateWorkspaceBlobReply{Blob: blob}, err
+	if _, err := sha.Write(out); err != nil {
+		return nil, err
 	}
 	sum := sha.Sum(nil)
 
 	return &CreateWorkspaceBlobReply{
-		Blob:           blob,
+		Blob:           out,
 		Sha256Checksum: sum,
 	}, nil
 }
