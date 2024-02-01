b2ef95a71d05186f52920f42d06547c165e1e781
itamar.marom <itamar.marom@appsflyer.com>
2024-02-01 15:10:15 +0200

    trigger ci

--- runner/server_plan.go
+++ runner/server_plan.go
@@ -147,7 +147,6 @@ func (r *TerraformRunnerServer) Plan(ctx context.Context, req *PlanRequest) (*Pl
 			log.Error(err, "The given tfvars file's path does not exists.")
 			return nil, err
 		}
-
 		planOpt = append(planOpt, tfexec.VarFile(secureTfVarsFile))
 	}
 
