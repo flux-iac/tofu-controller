10c818e1699fa6a697c5ee27789eaaba02d02ab1
aksel-skaar-leirvaag <aksel-skaar.leirvaag@telenor.no>
2024-05-31 21:05:22 +0200

    Speedup wait for pod ip

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -38,6 +38,7 @@ import (
 	flag "github.com/spf13/pflag"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrlcache "sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
@@ -220,6 +221,12 @@ func main() {
 		allowCrossNamespaceRefs = !aclOptions.NoCrossNamespaceRefs
 	}
 
+	clientset, err := kubernetes.NewForConfig(mgr.GetConfig())
+	if err != nil {
+		setupLog.Error(err, "unable to set up cert rotation")
+		os.Exit(1)
+	}
+
 	reconciler := &controllers.TerraformReconciler{
 		Client:                    mgr.GetClient(),
 		Scheme:                    mgr.GetScheme(),
@@ -234,6 +241,7 @@ func main() {
 		ClusterDomain:             clusterDomain,
 		NoCrossNamespaceRefs:      !allowCrossNamespaceRefs,
 		UsePodSubdomainResolution: usePodSubdomainResolution,
+		Clientset:                 clientset,
 	}
 
 	if err = reconciler.SetupWithManager(mgr, concurrent, httpRetry); err != nil {
