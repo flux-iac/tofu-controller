d5eeddc7061a05861f85ac0d6813b5bfd1b45f36
Daniel Ciaglia <daniel@sigterm.de>
2024-10-15 12:52:25 +0200

    switch hardcoded value to `false` (#1)

--- controllers/tf_controller_backend.go
+++ controllers/tf_controller_backend.go
@@ -344,7 +344,7 @@ terraform {
 
 	initRequest := &runner.InitRequest{
 		TfInstance: tfInstance,
-		Upgrade:    true,
+		Upgrade:    false, // ToDo: this should be configurable
 		ForceCopy:  true,
 		// Terraform:  terraformBytes,
 	}
