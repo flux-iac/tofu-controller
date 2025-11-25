05c421163a2d41298657369c5dad4ffa621b3883
Alex Marston <alexander.marston@imaginecurve.com>
2025-11-25 17:46:13 +0000

    update tests

--- controllers/tf_controller_interval_test.go
+++ controllers/tf_controller_interval_test.go
@@ -9,7 +9,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestShouldRespectPlanIntervalSkipsWhenIntervalNotElapsed(t *testing.T) {
+func TestShouldReconcileSkipsWhenIntervalNotElapsed(t *testing.T) {
 	g := NewWithT(t)
 	reconciler := &TerraformReconciler{}
 
@@ -35,22 +35,21 @@ func TestShouldRespectPlanIntervalSkipsWhenIntervalNotElapsed(t *testing.T) {
 	g.Expect(requeueAfter).To(BeNumerically("<=", 24*time.Hour))
 }
 
-func TestShouldRespectPlanIntervalAllowsReconcileOnChange(t *testing.T) {
+func TestShouldReconcileWhenIntervalElapsed(t *testing.T) {
 	g := NewWithT(t)
 	reconciler := &TerraformReconciler{}
 
-	lastPlan := time.Now().Add(-6 * time.Hour)
+	lastPlan := time.Now().Add(-25 * time.Hour)
 	tf := &infrav1.Terraform{
 		ObjectMeta: metav1.ObjectMeta{
-			Generation: 2,
+			Generation: 1,
 		},
 		Spec: infrav1.TerraformSpec{
 			Interval: metav1.Duration{Duration: 24 * time.Hour},
 		},
 		Status: infrav1.TerraformStatus{
-			LastPlanAt:            &metav1.Time{Time: lastPlan},
-			LastAttemptedRevision: "main/1234",
-			ObservedGeneration:    1,
+			LastPlanAt:         &metav1.Time{Time: lastPlan},
+			ObservedGeneration: 1,
 		},
 	}
 
@@ -59,7 +58,29 @@ func TestShouldRespectPlanIntervalAllowsReconcileOnChange(t *testing.T) {
 	g.Expect(requeueAfter).To(Equal(time.Duration(0)))
 }
 
-func TestShouldRespectPlanIntervalAllowsPendingApply(t *testing.T) {
+func TestShouldReconcileWhenGenerationChanged(t *testing.T) {
+	g := NewWithT(t)
+	reconciler := &TerraformReconciler{}
+
+	tf := &infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Generation: 2,
+		},
+		Spec: infrav1.TerraformSpec{
+			Interval: metav1.Duration{Duration: 24 * time.Hour},
+		},
+		Status: infrav1.TerraformStatus{
+			LastPlanAt:         &metav1.Time{Time: time.Now()},
+			ObservedGeneration: 1,
+		},
+	}
+
+	shouldReconcile, requeueAfter := reconciler.shouldReconcile(tf)
+	g.Expect(shouldReconcile).To(BeTrue())
+	g.Expect(requeueAfter).To(Equal(time.Duration(0)))
+}
+
+func TestShouldReconcileWhenPendingPlan(t *testing.T) {
 	g := NewWithT(t)
 	reconciler := &TerraformReconciler{}
 
@@ -76,6 +97,69 @@ func TestShouldRespectPlanIntervalAllowsPendingApply(t *testing.T) {
 			Plan: infrav1.PlanStatus{
 				Pending: "pending-plan",
 			},
+		},
+	}
+
+	shouldReconcile, requeueAfter := reconciler.shouldReconcile(tf)
+	g.Expect(shouldReconcile).To(BeTrue())
+	g.Expect(requeueAfter).To(Equal(time.Duration(0)))
+}
+
+func TestShouldReconcileWhenNeverPlanned(t *testing.T) {
+	g := NewWithT(t)
+	reconciler := &TerraformReconciler{}
+
+	tf := &infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Generation: 1,
+		},
+		Spec: infrav1.TerraformSpec{
+			Interval: metav1.Duration{Duration: 24 * time.Hour},
+		},
+	}
+
+	shouldReconcile, requeueAfter := reconciler.shouldReconcile(tf)
+	g.Expect(shouldReconcile).To(BeTrue())
+	g.Expect(requeueAfter).To(Equal(time.Duration(0)))
+}
+
+func TestShouldReconcileWhenDeleting(t *testing.T) {
+	g := NewWithT(t)
+	reconciler := &TerraformReconciler{}
+
+	now := metav1.Now()
+	tf := &infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Generation:        1,
+			DeletionTimestamp: &now,
+		},
+		Spec: infrav1.TerraformSpec{
+			Interval: metav1.Duration{Duration: 24 * time.Hour},
+		},
+		Status: infrav1.TerraformStatus{
+			LastPlanAt: &metav1.Time{Time: time.Now()},
+		},
+	}
+
+	shouldReconcile, requeueAfter := reconciler.shouldReconcile(tf)
+	g.Expect(shouldReconcile).To(BeTrue())
+	g.Expect(requeueAfter).To(Equal(time.Duration(0)))
+}
+
+func TestShouldReconcileWhenForceEnabled(t *testing.T) {
+	g := NewWithT(t)
+	reconciler := &TerraformReconciler{}
+
+	tf := &infrav1.Terraform{
+		ObjectMeta: metav1.ObjectMeta{
+			Generation: 1,
+		},
+		Spec: infrav1.TerraformSpec{
+			Interval: metav1.Duration{Duration: 24 * time.Hour},
+			Force:    true,
+		},
+		Status: infrav1.TerraformStatus{
+			LastPlanAt: &metav1.Time{Time: time.Now()},
 		},
 	}
 
